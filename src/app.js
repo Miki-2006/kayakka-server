0547eea46911dd370a6b5977e0425d0b1bb9f165
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-01 12:14:48 +0600

    Подготовка на deploy Vercel

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes/index.js'
 
 
@@ -6,6 +7,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 
 
