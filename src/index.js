b12b4754814bf58d9f582d9640e079bddadd7ebf
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-01 12:36:12 +0600

    Убрал listen

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const PORT = process.env.PORT;
 
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
-});
 
 export default app
