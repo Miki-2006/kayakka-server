9ba17fb1459347fea58e7af4be5a2f23e52c2384
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-03 14:25:27 +0600

    Изменил метод авторизации

--- src/config/dbConnecting.js
+++ src/config/dbConnecting.js
@@ -1,5 +1,4 @@
 import mssql from 'mssql'
-import { DefaultAzureCredential } from '@azure/identity'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -7,14 +6,12 @@ dotenv.config()
 
 
 
-const credential = new DefaultAzureCredential();
 
 const config = {
+  user: process.env.USER_MSSQL,
   server: process.env.SERVER_MSSQL,
   database: process.env.DATABASE_MSSQL,
-  authentication: {
-    type: "azure-active-directory-access-token",
-  },
+  password: process.env.PASSWORD_MSSQL,
   options: {
     trustServerCertificate: false,
     encrypt: true,
@@ -23,16 +20,6 @@ const config = {
 
 const connectToAzure = async () => {
   try {
-    const tokenResponse = await credential.getToken(
-      "https://database.windows.net/.default"
-    );
-    if (!tokenResponse || !tokenResponse.token) {
-      throw new Error("Ошибка получения токена доступа!");
-    }
-
-    config.authentication.options = {
-        token: tokenResponse.token
-    }
 
     const poolConnection = await mssql.connect(config);
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-app.listen(8000, () => {
-  console.log('http://localhost:3000/')
-})
 
 
-// export default app
+
+export default app
