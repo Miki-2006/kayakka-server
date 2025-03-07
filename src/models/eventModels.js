17997600543db08fa5df487a3d1fffd30653249b
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-07 13:00:00 +0600

    Обновил запрос

--- src/models/eventModels.js
+++ src/models/eventModels.js
@@ -3,7 +3,10 @@ import connectToAzure from "../config/dbConnecting.js";
 export const getAllEvents = async () => {
   const pool = await connectToAzure();
 
-  const result = await pool.request().query("SELECT title, description, price, image, event_date FROM Events");
+  const result = await pool
+    .request()
+    .query(
+      "SELECT e.title, l.venue, e.price, e.image, e.event_date FROM Events e JOIN Locations l ON e.location_id = l.id"
+    );
   return result.recordset;
 };
-
