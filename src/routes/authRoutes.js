288f70bf42284099cd6c1f54acbd3fe38d6fdb8c
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-21 12:19:33 +0600

    Коммит изменений

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,15 +8,15 @@ const router = express.Router()
 router.post('/register', register)
 router.get('/auto-login', (req, res) => {
     const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: 'Not logged in' });
+    if (!token) return res.status(401).json({ message: 'Не зарегистрированы' });
   
     jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
-      if (err) return res.status(401).json({ message: 'Invalid token' });
+      if (err) return res.status(401).json({ message: 'Не правильный токен' });
   
       const user = await User.findById(decoded.userId);
-      if (!user) return res.status(404).json({ message: 'User not found' });
+      if (!user) return res.status(404).json({ message: 'Пользователь не найден' });
   
-      res.json({ message: 'Auto login successful', user });
+      res.json({ message: 'Авто вход успешно закончен', user });
     });
   });
   
