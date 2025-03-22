baad31fd69a5277c978d8042494ae55db55303f8
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-22 11:56:49 +0600

    Обновил модель где нужно было импорт БД

--- src/models/loginModels.js
+++ src/models/loginModels.js
@@ -1,8 +1,9 @@
-import mssql from 'mssql'
+import connectToAzure from "../config/dbConnecting";
 
 export const getUserByEmail = async (email) => {
+  const pool = await connectToAzure();
   try {
-    const result = await mssql.query`SELECT * FROM Users WHERE email = ${email}`;
+    const result = await pool.query`SELECT * FROM Users WHERE email = ${email}`;
     return result.recordset[0]; // Возвращаем первый найденный результат
   } catch (err) {
     console.error('Error fetching user by email:', err.message);
