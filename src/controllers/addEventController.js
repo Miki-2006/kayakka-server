51c5e7ca3022ec3b09ffcfa56c1124eedc30549b
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-22 10:48:12 +0600

    Обновил кортроллер зпроса на добавления мероприятия

--- src/controllers/addEventController.js
+++ src/controllers/addEventController.js
@@ -1,23 +1,35 @@
-import Event from '../models/addEventModels.js'
+import Event from "../models/addEventModels.js";
 
 export const addEvent = async (req, res) => {
   try {
-    const { title, description, event_date, location_id, category_id, organizer_id, price, image } = req.body;
+    const {
+      title,
+      description,
+      event_date,
+      event_time,
+      category,
+      location,
+      organizer,
+      price,
+      image,
+    } = req.body;
 
-    // Проверяем, существуют ли локация и категория
-    const category = await Event.findCategory(category_id);
+    // Создаём событие с обработкой всех данных
+    const result = await Event.create({
+      title,
+      description,
+      event_date,
+      event_time,
+      category,
+      location,
+      organizer,
+      price,
+      image,
+    });
 
-    if (!category) {
-      return res.status(400).json({ error: "Категория не найдены" });
-    }
-
-    // Создаем мероприятие
-    await Event.create({ title, description, event_date, location_id, category_id, organizer_id, price, image });
-
-    res.json({ message: "Мероприятие добавлено!" });
+    res.json({ message: "Мероприятие добавлено!", event: result });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Ошибка при добавлении мероприятия" });
   }
 };
-
