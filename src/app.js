c31c65493ab44e44e226100e66f192719b1dbe03
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-28 16:33:03 +0600

    Мммnnn

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import routes from './routes/index.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import routes from "./routes/index.js";
 
+const app = express();
+const corsOptions = {
+  origin: "https://kayakka-client.vercel.app", // Разрешаем только этот источник
+  methods: ["GET", "POST", "PUT", "DELETE"], // Разрешаем нужные методы
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+   // Разрешаем заголовки
+};
 
-const app = express()
+app.use(express.json());
+app.use(
+  cors(corsOptions)
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use("/api", routes);
 
-app.use(express.json())
-app.use(cors({origin: 'https://kayakka-client.vercel.app/', credentials: true}))
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-
-app.use("/api", routes)
-
-app.get('/', (req, res) => {
-    res.send('Сервер работает! API доступен по https://kayakka-server-acbhlax8d-mirlans-projects.vercel.app//api/events');
+app.get("/", (req, res) => {
+  res.send(
+    "Сервер работает! API доступен по https://kayakka-server-acbhlax8d-mirlans-projects.vercel.app//api/events"
+  );
 });
 
-
-
-
-export default app
+export default app;
