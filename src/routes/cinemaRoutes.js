78d285e372bbf4929214483cb174cda5662e54ce
Miki-2006 <rys5ek0v.111@gmail.com>
2025-04-15 16:33:39 +0600

    Обновил роут запроса залов

--- src/routes/cinemaRoutes.js
+++ src/routes/cinemaRoutes.js
@@ -4,6 +4,6 @@ import { getCinemas, getHalls } from '../controllers/cinemaController.js'
 const router = express.Router();
 
 router.get('/', getCinemas)
-router.get('/halls/:selectedCinemaId', getHalls)
+router.get('/halls?selectedCinemaId', getHalls)
 
 export default router
